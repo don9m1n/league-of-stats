c00fb1592734cc546653bb1aebfe3db035c60e24
don9m1n <ddmkim94@gmail.com>
2024-11-12 13:04:37 +0900

    Init: 프로젝트 생성
    - .gitignore 작성
    - HELP.md 삭제

---
+++ src/main/java/com/leagueofstats/leagueofstats/LeagueOfStatsApplication.java
@@ -0,0 +1,13 @@
+package com.leagueofstats.leagueofstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfStatsApplication.class, args);
+	}
+
+}
